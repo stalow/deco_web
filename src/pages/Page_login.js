7ee72d6f27864250818865914dc20731a438d294
Hicham Benali <hicham.benali@lan.etictelecom.com>
2024-04-06 15:22:41 +0200

    first commit

---
+++ src/pages/Page_login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Page_login = () => {
+ 
+
+  return (
+    <div className='pagelog-wrapper'>
+      test
+    </div>
+  )
+}
+
+export default Page_login;
