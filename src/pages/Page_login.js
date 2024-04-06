7c4c5341825ff653656d4a923930b3cb72dd7495
Hicham Benali <hicham.benali@lan.etictelecom.com>
2024-04-06 15:49:02 +0200

    page login ok

--- src/pages/Page_login.js
+++ src/pages/Page_login.js
@@ -4,10 +4,27 @@ const Page_login = () => {
  
 
   return (
-    <div className='pagelog-wrapper'>
-      test
+    <div className='login-wrapper'>
+      <div className="login-container">
+          <h2>Connexion</h2>
+          <form className="login-form">
+              <div className="form-group">
+                  <label htmlFor="email">Email :</label>
+                  <input type="email" id="email" name="email" />
+              </div>
+              <div className="form-group">
+                  <label htmlFor="password">Mot de passe :</label>
+                  <input type="password" id="password" name="password" />
+              </div>
+              <button type="submit">Se connecter</button>
+          </form>
+          <div className="additional-options">
+              <p><a href="#">Mot de passe oublié ?</a></p>
+              <p><a href="#">Créer un compte</a></p>
+          </div>
+      </div>
     </div>
-  )
+    );
 }
 
 export default Page_login;
